a28703e15863a3e8a1ca26c380c10ea4a048d722
Hemant <hemantraj587@gmail.com>
2022-08-11 02:21:46 +0530

    :100:

--- Array/Easy_Problem/08_Find_indexSumofTwoElements.cpp
+++ Array/Easy_Problem/08_Find_indexSumofTwoElements.cpp
@@ -1,5 +1,5 @@
 // Input: 4
-//        2 7 11 15 
+//        2 7 11 15
 //        9
 // Output: [0,1]
 // Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
@@ -9,32 +9,53 @@
 // Output: [0,2]
 // Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int *arr,size,sum;
-    cout<<"Size :-  ";
-    cin>>size;
+    int *arr, size, sum;
+    cout << "Size :-  ";
+    cin >> size;
 
-    arr= new int[size];
+    arr = new int[size];
 
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    cout<<"Enter the sum of two elements :  ";
-    cin>>sum;
-    for(int x=0;x<size;x++)
+    cout << "Enter the sum of two elements :  ";
+    cin >> sum;
+
+    // O(N^2) solution
+
+    // for (int x = 0; x < size; x++)
+    // {
+    //     for (int y = x + 1; y < size; y++)
+    //     {
+    //         if (sum == arr[x] + arr[y])
+    //         {
+    //             cout << "[ " << x << " , " << y << " ]";
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // O(N)  solution
+
+    int first = 0, last = size - 1;
+    while (first < last)
     {
-        for(int y=x+1;y<size;y++)
+        if (sum == arr[first] + arr[last])
         {
-            if(sum==arr[x]+arr[y])
-            {
-                cout<<"[ "<<x<<" , "<<y<<" ]";
-                break;
-            }
+            cout << "[ " << first << " , " << last << " ]";
+            break;
         }
+        else if (sum > arr[first] + arr[last])
+        {
+            first = first + 1;
+        }
+        else
+            last -= 1;
     }
     return 0;
 }
