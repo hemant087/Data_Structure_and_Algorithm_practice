23c417132caa0a56127792e4489b659a10e6457c
Hemant <hemantraj587@gmail.com>
2022-08-12 00:47:29 +0530

    Kadane's Algo

--- Array/03_SubArray Problems/01_MaxSubArray_Sum.cpp
+++ Array/03_SubArray Problems/01_MaxSubArray_Sum.cpp
@@ -1,4 +1,3 @@
-// Given an integer array nums, find the contiguous sub array (containing at least one number) which has the largest sum and return its sum.
 // Input:
 //     9
 //     -2 1 -3 4 -1 2 1 -5 4
@@ -25,7 +24,30 @@ int main()
 
     int maxSum = INT_MIN;
 
+    //  Kadane's Algo   time compl = O[n]
+
+    int Curr_sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        Curr_sum += arr[i];
+        if (maxSum < Curr_sum)
+        {
+            maxSum = Curr_sum;
+        }
+        if (Curr_sum < 0)
+        {
+            Curr_sum = 0;
+        }
+    }
+    cout << "\n Your maximum sum of sub-array is :   " << maxSum;
+    return 0;
+}
+
+
+
+
     // brute force approach ,,,,,,,,,,,,,,time comp=O[n^3]
+
     // for (int x = 0; x < size; x++)
     // {
     //     for (int y = x; y < size; y++)
@@ -41,22 +63,19 @@ int main()
 
     // Cumulative sum approach ,,,,,,,,,,,,,,time comp=O[n^2]
 
-    int cumSum[size + 1];
-    cumSum[0] = 0;
-    for (int i = 1; i < size + 1; i++)
-    {
-        cumSum[i] = cumSum[i - 1] + arr[i - 1];
-    }
+    // int cumSum[size + 1];
+    // cumSum[0] = 0;
+    // for (int i = 1; i < size + 1; i++)
+    // {
+    //     cumSum[i] = cumSum[i - 1] + arr[i - 1];
+    // }
 
-    for (int x = 1; x < size + 1; x++)
-    {
-        int sum = 0;
-        for (int y = 0; y < x; y++)
-        {
-            sum = cumSum[x] - cumSum[y];
-            maxSum = max(maxSum, sum);
-        }
-    }
-    cout << "\n Your maximum sum of sub-array is :   " << maxSum;
-    return 0;
-}
+    // for (int x = 1; x < size + 1; x++)
+    // {
+    //     int sum = 0;
+    //     for (int y = 0; y < x; y++)
+    //     {
+    //         sum = cumSum[x] - cumSum[y];
+    //         maxSum = max(maxSum, sum);
+    //     }
+    // }
