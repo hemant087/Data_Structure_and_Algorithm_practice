3cf21936e943d41a7480f10cb9ae66a8601e09ca
harshanand23561 <72392089+harshanand23561@users.noreply.github.com>
2022-09-09 09:44:09 +0530

    New Recursion Problem

---
+++ Recursion/01_Palindrom.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+bool search(string str, int s, int e)
+{
+    if (s >= e)
+        return true;
+    if (str[s] != str[e])
+        return false;
+    return search(str, e--, s + 1);
+}
+
+int main()
+{
+    system("CLS");
+    string str;
+
+    cin >> str;
+    int n = str.size() - 1;
+
+    int x = search(str, 0, n);
+
+    if (x == 1)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
